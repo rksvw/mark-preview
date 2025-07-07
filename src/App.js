42acb73bbdcb59e817c94eca3a70a9362cfcae72
rksvw <ritik.vw98@gmail.com>
2025-07-08 00:10:44 +0530

    completed the second project markdow preview

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 import EditorComponent from "./Component/EditorComponent";
 import PreviewerComponent from "./Component/PreviewerComponent";
 
 function App() {
+  const [markdown, setMarkdown] = useState(
+    "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n"
+  );
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignContent: "center",
-        flexDirection: "column",
-        backgroundColor: "#87b5b5"
-      }}
-    >
-      <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <EditorComponent />
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          flexDirection: "column",
+          backgroundColor: "#87b5b5",
+          gap: "40px"
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <EditorComponent markdown={markdown} setMarkdown={setMarkdown} />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <PreviewerComponent markdown={markdown} />
+        </div>
       </div>
-      <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <PreviewerComponent />
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/Component/PreviewerComponent.js
+++ src/Component/PreviewerComponent.js
@@ -1,5 +1,24 @@
-import React from "react";
+import { marked } from "marked";
+import { FaExpandArrowsAlt } from "react-icons/fa";
+import { LiaFreeCodeCamp } from "react-icons/lia";
+import "./Preview.css";
 
-export default function PreviewerComponent() {
-  return <div>PreviewerComponent</div>;
+export default function PreviewerComponent({ markdown }) {
+  marked.setOptions({
+    breaks: true,
+  });
+  return (
+    <>
+      <div className="col-md-6 preview_card">
+        <div className="preview_toolbar">
+          <LiaFreeCodeCamp className="code_icon" /> Editor
+          <FaExpandArrowsAlt className="arrow_icon" />
+        </div>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        ></div>
+      </div>
+    </>
+  );
 }
